97191113d0b11c48da6b62217d3d8cecb8521257
black_rush <caasihapma@gmail.com>
2022-08-27 23:22:21 +0000

    fixing bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:farm_app/firebase_options.dart';
 import 'package:farm_app/src/home/presentation/pages/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MaterialApp(
     home: HomePage(),
     title: "Farm IOT",
--- lib/src/home/presentation/pages/homepage.dart
+++ lib/src/home/presentation/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:farm_app/src/home/presentation/widgets/custom_painter.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,12 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  DatabaseReference lightRef = FirebaseDatabase.instance.ref('Light');
+  DatabaseReference soilRef = FirebaseDatabase.instance.ref('moisture');
+  DatabaseReference tempRef = FirebaseDatabase.instance.ref('temp');
+  DatabaseReference waterRef = FirebaseDatabase.instance.ref('water');
+  DatabaseReference humidityRef = FirebaseDatabase.instance.ref('humidity');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,143 +28,249 @@ class HomePageState extends State<HomePage> {
         child: Column(children: [
           Row(
             children: [
-              CustomPaint(
-                foregroundPainter: CircleProgress(50, true, 100),
-                child: SizedBox(
-                  width: 190,
-                  height: 190,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text('Temperature'),
-                        Text(
-                          // '${tempAnimation.value.toInt()}',
-                          '50',
-                          style: TextStyle(
-                              fontSize: 50, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '°C',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              Expanded(
+                child: StreamBuilder<DatabaseEvent>(
+                    stream: tempRef.onValue,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return CustomPaint(
+                          foregroundPainter: CircleProgress(
+                              double.parse(
+                                  snapshot.data!.snapshot.value.toString()),
+                              true,
+                              100),
+                          child: SizedBox(
+                            width: 190,
+                            height: 190,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text('Temperature'),
+                                  Text(
+                                    // '${tempAnimation.value.toInt()}',
+                                    snapshot.data!.snapshot.value.toString(),
+                                    style: TextStyle(
+                                        fontSize: 50,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '°C',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    }),
               ),
-              CustomPaint(
-                foregroundPainter: CircleProgress(63, false, 100),
-                child: SizedBox(
-                  width: 190,
-                  height: 190,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text('Humidity'),
-                        Text(
-                          // '${tempAnimation.value.toInt()}',
-                          '68',
-                          style: TextStyle(
-                              fontSize: 50, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '%',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              Expanded(
+                child: StreamBuilder<DatabaseEvent>(
+                    stream: humidityRef.onValue,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return CustomPaint(
+                          foregroundPainter: CircleProgress(
+                              double.parse(
+                                  snapshot.data!.snapshot.value.toString()),
+                              false,
+                              100),
+                          child: SizedBox(
+                            width: 190,
+                            height: 190,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text('Humidity'),
+                                  Text(
+                                    // '${tempAnimation.value.toInt()}',
+                                    snapshot.data!.snapshot.value.toString(),
+                                    style: TextStyle(
+                                        fontSize: 50,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '%',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    }),
               ),
             ],
           ),
           Row(
             children: [
-              CustomPaint(
-                foregroundPainter: CircleProgress(50, false, 100),
-                child: SizedBox(
-                  width: 190,
-                  height: 190,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text('Water Level'),
-                        Text(
-                          // '${tempAnimation.value.toInt()}',
-                          '50',
-                          style: TextStyle(
-                              fontSize: 50, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '°C',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              Expanded(
+                child: StreamBuilder<DatabaseEvent>(
+                    stream: waterRef.onValue,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return CustomPaint(
+                          foregroundPainter: CircleProgress(
+                              double.parse(snapshot.data!.snapshot.value
+                                          .toString()
+                                          .split(',')[1]) >
+                                      100
+                                  ? 100
+                                  : double.parse(snapshot.data!.snapshot.value
+                                              .toString()
+                                              .split(',')[1]) <
+                                          0
+                                      ? 0
+                                      : double.parse(snapshot
+                                          .data!.snapshot.value
+                                          .toString()
+                                          .split(',')[1]),
+                              false,
+                              100),
+                          child: SizedBox(
+                            width: 190,
+                            height: 190,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text('Water Level'),
+                                  Text(
+                                    // '${tempAnimation.value.toInt()}',
+                                    double.parse(snapshot.data!.snapshot.value
+                                                .toString()
+                                                .split(',')[1]) >
+                                            100
+                                        ? '100'
+                                        : double.parse(snapshot
+                                                    .data!.snapshot.value
+                                                    .toString()
+                                                    .split(',')[1]) <
+                                                0
+                                            ? '0'
+                                            : snapshot.data!.snapshot.value
+                                                .toString()
+                                                .split(',')[1],
+                                    style: TextStyle(
+                                        fontSize: 50,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '%',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    }),
               ),
-              CustomPaint(
-                foregroundPainter: CircleProgress(63, true, 100),
-                child: SizedBox(
-                  width: 190,
-                  height: 190,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text('Soil Moisture'),
-                        Text(
-                          // '${tempAnimation.value.toInt()}',
-                          '68',
-                          style: TextStyle(
-                              fontSize: 50, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '%',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              Expanded(
+                child: StreamBuilder<DatabaseEvent>(
+                    stream: soilRef.onValue,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return CustomPaint(
+                          foregroundPainter: CircleProgress(
+                              double.parse(snapshot.data!.snapshot.value
+                                  .toString()
+                                  .split(',')[1]),
+                              true,
+                              100),
+                          child: SizedBox(
+                            width: 190,
+                            height: 190,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text('Soil Moisture'),
+                                  Text(
+                                    // '${tempAnimation.value.toInt()}',
+                                    snapshot.data!.snapshot.value
+                                        .toString()
+                                        .split(',')[1],
+                                    style: TextStyle(
+                                        fontSize: 50,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '%',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    }),
               ),
             ],
           ),
           Center(
-            child: CustomPaint(
-              foregroundPainter: CircleProgress(63, true, 100),
-              child: SizedBox(
-                width: 190,
-                height: 190,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget>[
-                      Text('Soil Moisture'),
-                      Text(
-                        // '${tempAnimation.value.toInt()}',
-                        '68',
-                        style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
+            child: StreamBuilder<DatabaseEvent>(
+                stream: lightRef.onValue,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return CustomPaint(
+                      foregroundPainter: CircleProgress(
+                          double.parse(snapshot.data!.snapshot.value
+                              .toString()
+                              .substring(0, 3)),
+                          true,
+                          100),
+                      child: SizedBox(
+                        width: 190,
+                        height: 190,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text('Amount of Light'),
+                              Text(
+                                // '${tempAnimation.value.toInt()}',
+                                snapshot.data!.snapshot.value
+                                    .toString()
+                                    .substring(0, 3),
+                                style: TextStyle(
+                                    fontSize: 50, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '%',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        '%',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+                    );
+                  } else
+                    return CircularProgressIndicator();
+                }),
           ),
           Row(
             children: [
